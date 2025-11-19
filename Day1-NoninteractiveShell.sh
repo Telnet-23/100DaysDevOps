525ffc2aaa2997a81e7ba3c0332a3a30b0c83a4c
Telnet-23 <122405584+Telnet-23@users.noreply.github.com>
2025-11-19 12:58:47 +0000

    Corrected some typos and added the option to give the user sudo permissions

--- Day1-NoninteractiveShell.sh
+++ Day1-NoninteractiveShell.sh
@@ -1,26 +1,48 @@
 #!/bin/bash
 
-#Get users name
+##########################
+## Version 2            ##
+## Date 19/11/25        ##
+## Author: Terry Knight ##
+##########################
+
+# Description: Creates a new user, sets the shell and the password and allows sudoer permission
+
+# Get users name
 read -p "What is the users name? " name
 
-#Set user shell
-read -p "Does the user require an interactive shell? [y/n] " shell
+# Set user shell
+read -p "Does the user require an interactive shell? [y/n]: " shell
 
-#Add the user
+# Add the user
 adduser $name
 
-#set the shell
-while [ "$shell"!= "y" ] && [ "$shell"!= "n"]; do
-	read -p "Please state y or no. Does the user require an interactive shell? " shell
+# set the shell and password
+while [ "$shell" != "y" ] && [ "$shell" != "n" ]; do
+	read -p "Please state y or n. Does the user require an interactive shell? " shell
 done
 
 if [ "$shell" = "y" ]; then
 	usermod -s /bin/bash $name
-	read -p "Please enter a password: " password
-	passwd $name $password
+	echo "Please enter a password"
+	passwd $name
 else
 	usermod -s /sbin/nologin $name
 fi
 
-echo "user $created"
+# Add Sudoer permission
+read -p "Does the user require Sudo permissions? [y/n]: " permission
+
+while [ "$permission" != "y" ] && [ "$permission" != "n" ]; do
+	read -p "Please stay y or n. Does the user require Sudo permissions? [y/n]: " permission
+done
+
+if [ "$permission" = "y"]; then
+	usermod -aG wheel $name
+else
+	echo "User does not have Sudo permissions"
+fi
+
+# Confirm completion
+echo "user $name created"
 	
